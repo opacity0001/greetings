f0bb4b1f3550aff7a310e4c6976a5a59b4754419
opacity0001 <opacity0001@gmail.com>
2022-03-27 07:51:59 +0900

    2rd

--- greetings.go
+++ greetings.go
@@ -1,6 +1,6 @@
 package greetings
 import "fmt"
-//Hello returns a greeting for the named person.
+// Hello returns a greeting for the named person.
 func Hello(name string) string {
 	//Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
